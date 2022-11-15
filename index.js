99ef4d238cb2fae4dbd3c113e50a0fb4c45f8002
Johannes <johannesfeige@gmail.com>
2022-11-15 20:10:38 +0100

    stuff

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// General-purpose functions
 const trush = (x) => (f) => f(x);
+const tap = (f) => (x) => {
+  f(x);
+  return x;
+};
+const pipe = (...fs) => (x) => fs.reduce((acc, f) => f(acc), x);
+const mult = (x) => (y) => x * y;
 
+const Rx = {};
+Rx.map = (f) => new Mapper(f);
+
+const double = mult(2);
+
+// Reactive library
 class Observable {
   constructor() {
     this.cbs = [];
@@ -12,27 +25,41 @@ class Observable {
   emit(x) {
     this.cbs.map(trush(x));
   }
+
+  pipe(o) {
+    this.subscribe((x) => o.emit(x));
+    return o;
+  }
 }
 
-const observable = new Observable();
+class Mapper {
+  constructor(f) {
+    this.observable = new Observable();
+    this.f = f;
+  }
+  subscribe(cbs) {
+    this.observable.subscribe(cbs);
+  }
+  emit(x) {
+    this.observable.emit(this.f(x));
+  }
+}
 
-const tap = (f) => (x) => {
-  f(x);
-  return x;
-};
-const pipe = (...fs) => (x) => fs.reduce((acc, f) => f(acc), x);
+// Example
+const original = new Observable();
 
-const double = (x) => x * 2;
-observable.subscribe(
-  pipe(
-    double,
-    tap(console.log),
-    tap(console.log)
-  )
-);
+const doubled = original.pipe(Rx.map(mult(2)));
+// const doubler = original.pipe(new Mapper((x) => x * 2));
 
-observable.emit(10);
-observable.emit(1);
-observable.emit(2);
-observable.emit(3);
-observable.emit(4);
+// const doubleAbs = original.pipe(
+//   new Mapper((x) => x * 2),
+//   new Mapper(Math.abs)
+// );
+
+original.subscribe(console.log);
+// doubler.subscribe(console.log);
+// doubleThenAddTen.subscribe(console.log);
+
+original.emit(10);
+original.emit(-1);
+original.emit(1);
